2cc67218e1764200082add7152ec61679a3ce5d3
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-04-24 16:14:51 +0200

    feat(yaml): implement YAML data printing functionality
    Added a new file to handle the printing of philosopher data in YAML format.
    This improves data representation and allows for easier debugging and analysis.

---
+++ philo/print_yaml.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_yaml.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 16:01:19 by lfiorell          #+#    #+#             */
+/*   Updated: 2025/04/24 16:03:17 by lfiorell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	print_data_overview(t_data *d)
+{
+	printf("data:\n");
+	printf("  num_philos: %d\n", d->num_philos);
+	printf("  time_to_eat: %d\n", d->time_to_eat);
+	printf("  time_to_sleep: %d\n", d->time_to_sleep);
+	printf("  time_to_die: %d\n", d->time_to_die);
+	printf("  max_meals: %d\n", d->max_meals);
+	printf("  time: %ld\n", d->time);
+	printf("  dead_people: %d\n", d->dead_people);
+}
+
+static void	print_philos_list(t_data *d)
+{
+	int		i;
+	t_philo	*p;
+	int		idx;
+
+	printf("  philos:\n");
+	for (i = 0; i < d->num_philos; ++i)
+	{
+		p = d->philos[i];
+		idx = p->id - 1;
+		printf("    - id: %d\n", p->id);
+		printf("      last_meal_time: %ld\n", p->last_meal_time);
+		printf("      meals_eaten: %d\n", p->meals_eaten);
+		printf("      left_fork: fork%d\n", idx);
+		printf("      right_fork: fork%d\n", (idx + 1) % d->num_philos);
+	}
+}
+
+static void	print_forks_list(t_data *d)
+{
+	int	i;
+
+	printf("  forks:\n");
+	for (i = 0; i < d->num_philos; ++i)
+		printf("    - fork%d\n", i);
+}
+
+static void	print_mutexes(void)
+{
+	printf("  print_mutex: print_mutex\n");
+	printf("  time_mutex: time_mutex\n");
+}
+
+static void	print_addresses(t_data *d)
+{
+	int	i;
+
+	printf("addresses:\n");
+	for (i = 0; i < d->num_philos; ++i)
+		printf("  fork%d: %p\n", i, (void *)d->forks[i]);
+	printf("  print_mutex: %p\n", (void *)d->print_mutex);
+	printf("  time_mutex: %p\n", (void *)d->time_mutex);
+}
+
+void	print_data_yaml(t_data *d)
+{
+	print_data_overview(d);
+	print_philos_list(d);
+	print_forks_list(d);
+	print_mutexes();
+	print_addresses(d);
+}
