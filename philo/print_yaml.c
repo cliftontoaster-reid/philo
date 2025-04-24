868a21224435991974db8dcf7a8d3173732dcb08
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-04-24 16:25:01 +0200

    fix(print_yaml): correct loop structure for printing
    Updated the loop structure in print_philos_list and print_forks_list
    to use while instead of for for better clarity. Removed the unused
    print_mutexes function and directly printed mutex information in
    print_data_yaml for improved readability.

--- philo/print_yaml.c
+++ philo/print_yaml.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:01:19 by lfiorell          #+#    #+#             */
-/*   Updated: 2025/04/24 16:03:17 by lfiorell         ###   ########.fr       */
+/*   Updated: 2025/04/24 16:24:13 by lfiorell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ static void	print_philos_list(t_data *d)
 	int		idx;
 
 	printf("  philos:\n");
-	for (i = 0; i < d->num_philos; ++i)
+	i = 0;
+	while (i < d->num_philos)
 	{
 		p = d->philos[i];
 		idx = p->id - 1;
@@ -40,6 +41,7 @@ static void	print_philos_list(t_data *d)
 		printf("      meals_eaten: %d\n", p->meals_eaten);
 		printf("      left_fork: fork%d\n", idx);
 		printf("      right_fork: fork%d\n", (idx + 1) % d->num_philos);
+		++i;
 	}
 }
 
@@ -48,14 +50,12 @@ static void	print_forks_list(t_data *d)
 	int	i;
 
 	printf("  forks:\n");
-	for (i = 0; i < d->num_philos; ++i)
+	i = 0;
+	while (i < d->num_philos)
+	{
 		printf("    - fork%d\n", i);
-}
-
-static void	print_mutexes(void)
-{
-	printf("  print_mutex: print_mutex\n");
-	printf("  time_mutex: time_mutex\n");
+		++i;
+	}
 }
 
 static void	print_addresses(t_data *d)
@@ -63,8 +63,12 @@ static void	print_addresses(t_data *d)
 	int	i;
 
 	printf("addresses:\n");
-	for (i = 0; i < d->num_philos; ++i)
+	i = 0;
+	while (i < d->num_philos)
+	{
 		printf("  fork%d: %p\n", i, (void *)d->forks[i]);
+		++i;
+	}
 	printf("  print_mutex: %p\n", (void *)d->print_mutex);
 	printf("  time_mutex: %p\n", (void *)d->time_mutex);
 }
@@ -74,6 +78,7 @@ void	print_data_yaml(t_data *d)
 	print_data_overview(d);
 	print_philos_list(d);
 	print_forks_list(d);
-	print_mutexes();
+	printf("  print_mutex: print_mutex\n");
+	printf("  time_mutex: time_mutex\n");
 	print_addresses(d);
 }
