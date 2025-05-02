fe81179fc3f48510fc1e945c9bca8ef6c0de74fd
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-05-02 12:37:24 +0200

    refactor(cleanup): simplify cleanup logic
    Extracted the logic for freeing philosopher resources into a separate
    function to improve code readability and maintainability. This change
    reduces duplication and clarifies the cleanup process for forks and
    philosophers.

--- philo/cleanup.c
+++ philo/cleanup.c
@@ -6,11 +6,23 @@
 /*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:57:30 by lfiorell          #+#    #+#             */
-/*   Updated: 2025/04/15 15:04:23 by lfiorell         ###   ########.fr       */
+/*   Updated: 2025/05/02 12:36:15 by lfiorell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static inline void	free_philo(t_data *data, int i)
+{
+	if (data->forks[i] != NULL)
+	{
+		pthread_mutex_destroy(data->forks[i]);
+		free(data->forks[i]);
+	}
+	if (data->philos[i] != NULL)
+		free(data->philos[i]);
+	i++;
+}
 
 void	cleanup(t_data *data)
 {
@@ -18,20 +30,20 @@ void	cleanup(t_data *data)
 
 	i = 0;
 	while (i < data->num_philos)
-	{
-		if (data->forks[i] != NULL)
-			pthread_mutex_destroy(data->forks[i]);
-		if (data->philos[i] != NULL)
-			free(data->philos[i]);
-		i++;
-	}
+		free_philo(data, i++);
 	if (data->forks != NULL)
 		free(data->forks);
 	if (data->philos != NULL)
 		free(data->philos);
 	if (data->print_mutex != NULL)
+	{
 		pthread_mutex_destroy(data->print_mutex);
+		free(data->print_mutex);
+	}
 	if (data->time_mutex != NULL)
+	{
 		pthread_mutex_destroy(data->time_mutex);
+		free(data->time_mutex);
+	}
 	free(data);
 }
